645aefdc1e97f35534adc4e1f48f7436504ce9f4
Aishah Alharthi <aishah.alharthi@kcl.ac.uk>
2025-07-02 10:28:21 +0300

    Feat: Added global variables file for easier control

---
+++ constants.py
@@ -0,0 +1,11 @@
+START = 58384
+# END = 114383
+END = 58484
+# BATCH_SIZE = 1000
+BATCH_SIZE = 10
+
+TEMPLATE_PATH = 'Rebuilt Base Template.svg'
+OUTPUT_DIR = 'outputs'
+NUMBER = '0058384'
+URL = f'https://app.netzero.sa/tag/{NUMBER}'
+QR_SIZE_PX = 43.8
