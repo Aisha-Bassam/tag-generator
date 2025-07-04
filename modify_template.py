2e2115b80d4ec9644751ae3496a8a4c25ba26d3e
Aishah Alharthi <aishah.alharthi@kcl.ac.uk>
2025-07-04 03:46:39 +0300

    Attempt to match the URL with the original template.

--- modify_template.py
+++ modify_template.py
@@ -5,15 +5,22 @@ import re
 input_file = "Master Template SVG.svg"
 output_file = "Rebuilt Base Template.svg"
 
+# # Register SVG namespace
+# SVG_NS = "http://www.w3.org/2000/svg"
+# ET.register_namespace('', SVG_NS)
+
 # Register SVG namespace
 SVG_NS = "http://www.w3.org/2000/svg"
+AI_NS = "http://ns.adobe.com/AdobeIllustrator/10.0/"
 ET.register_namespace('', SVG_NS)
+ET.register_namespace('i', AI_NS)  # force Illustrator-compatible prefix
 
 # Parse input SVG
 tree = ET.parse(input_file)
 root = tree.getroot()
-root.attrib['width'] = '7cm'
-root.attrib['height'] = '2.5cm'
+# root.attrib['width'] = '7cm'
+# root.attrib['height'] = '2.5cm'
+# root.attrib['scale'] = '0.1'
 # root.attrib['viewBox'] = '0 0 264.6 94.5'  # 7cm × 2.5cm at 37.8 px/cm
 
 
